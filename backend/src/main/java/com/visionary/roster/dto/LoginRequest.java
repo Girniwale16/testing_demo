667563b535dde18cfd3cadb8935e3d0551e9edd1
Girniwale16 <akshaya.girniwale@nitorinfotech.com>
2026-08-24 11:46:22 +0530

    VIS-2094

---
+++ backend/src/main/java/com/visionary/roster/dto/LoginRequest.java
@@ -0,0 +1,22 @@
+package com.visionary.roster.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequest {
+
+    @NotBlank(message = "Username is required")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    @NotNull(message = "Facility ID is required")
+    private Long facilityId;
+}
