782c1912807db87a9013ede16e80648615051969
Girniwale16 <akshaya.girniwale@nitorinfotech.com>
2026-08-24 11:46:23 +0530

    VIS-2094

---
+++ backend/src/main/java/com/visionary/roster/dto/LoginResponse.java
@@ -0,0 +1,20 @@
+package com.visionary.roster.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LoginResponse {
+
+    private Long userId;
+    private String username;
+    private String role;
+    private Long facilityId;
+    private String facilityName;
+    private String message;
+}
