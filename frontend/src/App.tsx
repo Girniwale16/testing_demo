fbf47e8f5ba2e864220096d4d8ef0316cf011f76
Girniwale16 <akshaya.girniwale@nitorinfotech.com>
2026-08-24 10:55:01 +0530

    VIS-2092

---
+++ frontend/src/App.tsx
@@ -0,0 +1,32 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import ProtectedRoute from './components/ProtectedRoute';
+import { logger } from './utils/logger';
+import { useEffect } from 'react';
+
+function App() {
+  useEffect(() => {
+    logger.info('App mounted', { event: 'app_mount' });
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/*"
+          element={
+            <ProtectedRoute>
+              <div style={{ padding: '2rem' }}>
+                <h1>Protected Content</h1>
+                <p>You are authenticated. Protected routes will be added here.</p>
+              </div>
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
